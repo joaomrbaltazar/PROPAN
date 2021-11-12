c8082d8ca081add8216907ea74dd6458a9e1da94
João Baltazar <93785445+joaomrbaltazar@users.noreply.github.com>
2021-11-12 14:42:35 +0000

    Update bladegeom.f90

--- PROPAN_Source_Code/Geom/bladegeom.f90
+++ PROPAN_Source_Code/Geom/bladegeom.f90
@@ -1,6 +1,6 @@
 !-----------------------------------------------------------------------------------------------!
 !    Compute Centroid Unit Normal and Area                                                      !
-!    Copyright (C) 2021  J. Baltazar                                                            !
+!    Copyright (C) 2021  J. Baltazar and J.A.C. Falcão de Campos                                !
 !                                                                                               !
 !    This program is free software: you can redistribute it and/or modify it under the terms of !
 !    the GNU Affero General Public License as published by the Free Software Foundation, either !
