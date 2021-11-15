c317eae33dc50ba88a68f33c780bf46bc7083e9f
João Baltazar <93785445+joaomrbaltazar@users.noreply.github.com>
2021-11-15 12:37:28 +0000

    Update bladevelo.f90

--- PROPAN_Source_Code/InfCoef/VelCoef/bladevelo.f90
+++ PROPAN_Source_Code/InfCoef/VelCoef/bladevelo.f90
@@ -1,5 +1,5 @@
 !-----------------------------------------------------------------------------------------------!
-!    Copyright (C) 2021  J. Baltazar and J.A.C. Falcão de Campos                                !
+!    Copyright (C) 2021  J. Baltazar                                                            !
 !                                                                                               !
 !    This program is free software: you can redistribute it and/or modify it under the terms of !
 !    the GNU Affero General Public License as published by the Free Software Foundation, either !
