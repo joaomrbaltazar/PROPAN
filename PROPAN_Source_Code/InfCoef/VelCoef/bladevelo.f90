e918aafa002e9aada8b746a10ca4481203288007
João Baltazar <93785445+joaomrbaltazar@users.noreply.github.com>
2021-11-15 12:38:21 +0000

    Update bladevelo.f90

--- PROPAN_Source_Code/InfCoef/VelCoef/bladevelo.f90
+++ PROPAN_Source_Code/InfCoef/VelCoef/bladevelo.f90
@@ -1,4 +1,5 @@
 !-----------------------------------------------------------------------------------------------!
+!    Blade influence coefficients                                                               !
 !    Copyright (C) 2021  J. Baltazar                                                            !
 !                                                                                               !
 !    This program is free software: you can redistribute it and/or modify it under the terms of !
