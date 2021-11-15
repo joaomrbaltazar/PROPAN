e3b1fa8c09ded33fbb9ad045193ce67ae70d9ce6
João Baltazar <93785445+joaomrbaltazar@users.noreply.github.com>
2021-11-15 13:37:21 +0000

    Update SolveIpkcWet.f90

--- PROPAN_Source_Code/Solve/SolveIpkcWet.f90
+++ PROPAN_Source_Code/Solve/SolveIpkcWet.f90
@@ -1,5 +1,5 @@
 !-----------------------------------------------------------------------------------------------!
-!    Solve Wetted Iterative Pressure Kutta Condition                                            !
+!    Solve wetted iterative pressure Kutta condition                                            !
 !    Copyright (C) 2021  J. Baltazar                                                            !
 !                                                                                               !
 !    This program is free software: you can redistribute it and/or modify it under the terms of !
