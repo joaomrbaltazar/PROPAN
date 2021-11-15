62fbcea30c7cede672faaa944270aad34003ce9b
João Baltazar <93785445+joaomrbaltazar@users.noreply.github.com>
2021-11-15 13:36:58 +0000

    Update SolveLkCav.f90

--- PROPAN_Source_Code/Solve/SolveLkCav.f90
+++ PROPAN_Source_Code/Solve/SolveLkCav.f90
@@ -1,5 +1,5 @@
 !-----------------------------------------------------------------------------------------------!
-!    Solve Cavitation Linear Kutta Condition                                                    !
+!    Solve cavitation linear Kutta condition                                                    !
 !    Copyright (C) 2021  J. Baltazar and J.A.C. Falcão de Campos                                !
 !                                                                                               !
 !    This program is free software: you can redistribute it and/or modify it under the terms of !
