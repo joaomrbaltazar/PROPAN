301c2d9b5d9e33dcaaf3edd577d3bb80e77cc8f9
João Baltazar <93785445+joaomrbaltazar@users.noreply.github.com>
2021-11-17 12:17:11 +0000

    Update propanel_mod.f90

--- PROPANEL_Source_Code/Base/propanel_mod.f90
+++ PROPANEL_Source_Code/Base/propanel_mod.f90
@@ -1,5 +1,5 @@
 !-----------------------------------------------------------------------------------------------!
-!    PROPANEL module                                                                            !
+!    PROPANEL Module                                                                            !
 !    Copyright (C) 2021  J. Baltazar                                                            !
 !                                                                                               !
 !    This program is free software: you can redistribute it and/or modify it under the terms of !
