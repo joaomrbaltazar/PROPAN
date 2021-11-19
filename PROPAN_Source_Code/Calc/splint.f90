2fe2873598e02f3df95bd0f7465c9c631852facb
João Baltazar <93785445+joaomrbaltazar@users.noreply.github.com>
2021-11-19 14:45:29 +0000

    Update splint.f90

--- PROPAN_Source_Code/Calc/splint.f90
+++ PROPAN_Source_Code/Calc/splint.f90
@@ -1,4 +1,17 @@
 !-----------------------------------------------------------------------------------------------!
+!    Copyright (C) 2021  J. Baltazar                                                            !
+!                                                                                               !
+!    This program is free software: you can redistribute it and/or modify it under the terms of !
+!    the GNU Affero General Public License as published by the Free Software Foundation, either !
+!    version 3 of the License, or (at your option) any later version.                           !
+!                                                                                               !
+!    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;  !
+!    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  !
+!    See the GNU Affero General Public License for more details.                                !
+!                                                                                               !
+!    You should have received a copy of the GNU Affero General Public License                   !
+!    along with this program.  If not, see <https://www.gnu.org/licenses/>.                     !
+!-----------------------------------------------------------------------------------------------!
 SUBROUTINE SPLINT(NDATA,X,Z,N,XX,ZZ)
 !-----------------------------------------------------------------------------------------------!
 !    Author: 07060213, J. Baltazar                                                              !
