ec33c0f7316ae63244e6638f834f2affbdb8e74d
João Baltazar <93785445+joaomrbaltazar@users.noreply.github.com>
2021-11-19 17:36:15 +0000

    Create propost.sh

---
+++ PROPOST_Source_Code/propost.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# clean
+##rm -f Code/*.*
+##rm -f ProPost2020_v1.0_debug.out
+# Source Folder
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/propost_mod.f90
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/ProPost2020_v1.0.f90
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/progress.f90
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/input.f90
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/delvars.f90
+# Plots Folder
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Plots/plotcp.f90
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Plots/plotharm.f90
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Plots/plotwake.f90
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Plots/plotsol.f90
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Plots/plotcav.f90
+# Calc Folder
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Calc/linint.f90
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Calc/intk1.f90
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Calc/splint.f90
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Calc/spline.f90
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Calc/ispline.f90
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Calc/periodicflow.f90
+# Viscor Folder
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Visc/viscor.f90
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Visc/clcd.f90
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Visc/interval.f90
+# Geom Folder
+##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Geom/panel.f90
+# Executable
+##ifort -o ProPost2020_v1.0_debug.out *.o
+##mv *.o Code
+##mv *.mod Code
+##if [ -f ProPost2020_v1.0_debug.out ]; then
+##   cp ProPost2020_v1.0_debug.out Code
+##fi
