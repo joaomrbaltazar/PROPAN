f22d9b1723740fe7e15b534d924ee88446bd87f7
João Baltazar <93785445+joaomrbaltazar@users.noreply.github.com>
2021-11-25 17:06:53 +0000

    Update propost.sh

--- PROPOST_Source_Code/propost.sh
+++ PROPOST_Source_Code/propost.sh
@@ -16,16 +16,16 @@ ifort -c -check bounds -traceback -fltconsistency -fpe0 Plots/plotwake.f90
 ifort -c -check bounds -traceback -fltconsistency -fpe0 Plots/plotsol.f90
 ifort -c -check bounds -traceback -fltconsistency -fpe0 Plots/plotcav.f90
 # Calc Folder
-##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Calc/linint.f90
-##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Calc/intk1.f90
-##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Calc/splint.f90
-##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Calc/spline.f90
-##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Calc/ispline.f90
-##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Calc/periodicflow.f90
+ifort -c -check bounds -traceback -fltconsistency -fpe0 Calc/linint.f90
+ifort -c -check bounds -traceback -fltconsistency -fpe0 Calc/intk1.f90
+ifort -c -check bounds -traceback -fltconsistency -fpe0 Calc/splint.f90
+ifort -c -check bounds -traceback -fltconsistency -fpe0 Calc/periodicflow.f90
+cp Calc/spline.o .
+cp Calc/ispline.o .
 # Visc Folder
-ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Visc/viscor.f90
-ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Visc/clcd.f90
-ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Visc/interval.f90
+ifort -c -check bounds -traceback -fltconsistency -fpe0 Visc/viscor.f90
+ifort -c -check bounds -traceback -fltconsistency -fpe0 Visc/clcd.f90
+ifort -c -check bounds -traceback -fltconsistency -fpe0 Visc/interval.f90
 # Geom Folder
 ifort -c -check bounds -traceback -fltconsistency -fpe0 Geom/panel.f90
 # Executable
