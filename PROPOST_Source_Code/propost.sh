8af011406ee755281129308892ffc4f4519f4dda
João Baltazar <93785445+joaomrbaltazar@users.noreply.github.com>
2021-11-25 14:12:31 +0000

    Update propost.sh

--- PROPOST_Source_Code/propost.sh
+++ PROPOST_Source_Code/propost.sh
@@ -22,7 +22,7 @@ ifort -c -check bounds -traceback -fltconsistency -fpe0 Plots/plotcav.f90
 ##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Calc/spline.f90
 ##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Calc/ispline.f90
 ##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Calc/periodicflow.f90
-# Viscor Folder
+# Visc Folder
 ##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Visc/viscor.f90
 ##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Visc/clcd.f90
 ##ifort -c -check bounds -traceback -fltconsistency -fpe0 Source/Visc/interval.f90
